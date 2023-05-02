20ab3c803c5a37f9460ccf22eca940886029ac62
nova1113 <nova9353@gmail.com>
2023-05-02 15:56:23 +0900

    P27979 Time Over

--- 백준/1번 문제/src/P27979.java
+++ 백준/1번 문제/src/P27979.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class P27979 {
     public static void main(String[] args) {
@@ -51,6 +53,7 @@ public class P27979 {
     private static List<Integer> substractMin(List<Integer> list) {
         int minIndex = 0;
         int minValue = list.get(0);
+        Hashtable<Integer, Stack<Integer>> hashtable = new Hashtable<>();
     
         for (int i = list.size() - 1; i >= 0 ; i--) {
             if (list.get(i) < minValue) {
